0245da20718a0caa05a593ae543dc15c831d3999
Robin Ridderholt <robin.ridderholt@live.com>
2014-02-03 22:34:13 +0100

    Initial commit

---
+++ TestConverter/Converter.fs
@@ -0,0 +1,32 @@
+﻿
+module Converter
+    let parseLine (line:string, className:string) =
+        let str = line.Trim() 
+        match str with
+        | "[TestMethod]" -> "\t\t[Fact]"
+        | "[ClassInitialize]" -> ""
+        | "[TestInitialize]" -> ""
+        | "[TestClass]" -> ""
+        | "base.Setup();" -> ""
+        | "public override void Setup()" -> ["\t\t";"public "; className; "()"] |> String.concat ""
+        | _ -> line
+
+    let containsName (line:string) = 
+        let isClassName = line.Trim().StartsWith("public class")         
+        isClassName
+
+    let removeClass (line:string) =
+        let cleaned = line.Trim().Replace("public class ", "")
+        cleaned
+
+    let removeBase (line:string) = 
+        let cleaned = line.Trim().Replace(" : ServiceTestBase", "") 
+        cleaned
+            
+    let extractName (line:string) = 
+        let name = line.Trim() |> removeClass |> removeBase
+        name
+               
+
+                
+
---
+++ TestConverter/Program.fs
@@ -0,0 +1,17 @@
+﻿open System
+open Converter
+
+
+
+[<EntryPoint>]
+let main argv = 
+    let path = "<REPLACE WITH PATH TO TEST FILE>"
+
+    let rows = System.IO.File.ReadAllLines(path)
+    let className = rows |> Array.find containsName |> extractName
+    let transformed = rows |> Array.map (fun line -> parseLine(line, className))
+
+    System.IO.File.WriteAllLines(path, transformed)
+
+    0 
+
